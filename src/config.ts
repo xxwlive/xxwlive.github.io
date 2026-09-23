0dcf3ea30001b3eed4824262bbf79713321e000d
xxwlive <2807459163@qq.com>
2026-09-23 14:41:38 +0800

    11

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "遥远612星球",
-	subtitle: "夏祥伟的博客",
+	subtitle: "xxw的博客",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -54,7 +54,7 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/me.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "夏祥伟",
+	name: "xxw",
 	bio: "华中科技大学电子信息在读硕士，研究无人机/机器人系统与大模型 Agent 工程化。",
 	links: [
 		{
